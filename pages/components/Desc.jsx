549e9fa1d9035c1561ed70dbe7ebab9ba4976871
Lekan Dar <Lekandar11@gmail.com>
2022-03-20 18:11:20 -1200

    version-1

---
+++ pages/components/Desc.jsx
@@ -0,0 +1,73 @@
+import {
+  Box,
+  Heading,
+  Button,
+  Flex,
+  Image,
+  Stack,
+  Text,
+  IconButton,
+} from "@chakra-ui/react";
+import { AiFillSafetyCertificate } from "react-icons/ai";
+import { FaRegChartBar } from "react-icons/fa";
+import { GiSuspensionBridge } from "react-icons/gi";
+
+function Desc() {
+  return (
+    <>
+      <Box py="20px">
+        <Heading
+          textTransform="uppercase"
+          color="#393E46"
+          textAlign={{ base: "center", lg: "left" }}
+        >
+          who we are
+        </Heading>
+        <Flex
+          justify="space-between"
+          align="center"
+          py={{ base: "25px", lg: "50px" }}
+          flexDir={{ base: "column", md: "row", lg: "row" }}
+          color="#393E46"
+        >
+          <Box width="350px" shadow="md" px="10px" py="50px">
+            <IconButton
+              textAlign="center"
+              colorScheme="blue"
+              aria-label="Search database"
+              icon={<GiSuspensionBridge size="26px" />}
+            />
+            <Text mt="10px">
+              we are your bridge to that technological break and innovative
+              concept you seek in the digital and I.T space
+            </Text>
+          </Box>
+          <Box width="350px" shadow="md" px="10px" py="50px">
+            <IconButton
+              colorScheme="blue"
+              aria-label="Search database"
+              icon={<FaRegChartBar size="26px" />}
+            />
+            <Text mt="10px">
+              we brand your trend through our aesthetically pleasing visual
+              representation and brand management consulting services
+            </Text>
+          </Box>
+          <Box width="350px" shadow="md" px="10px" py="50px">
+            <IconButton
+              colorScheme="blue"
+              aria-label="Search database"
+              icon={<AiFillSafetyCertificate size="26px" />}
+            />
+            <Text mt="10px">
+              we are your guide, and provide a safe landing for all your social
+              media needs and queries
+            </Text>
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
+}
+
+export default Desc;
---
+++ pages/components/Hero.jsx
@@ -0,0 +1,47 @@
+import {
+  Box,
+  Heading,
+  Button,
+  Flex,
+  Image,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+
+function Hero() {
+  return (
+    <>
+      <Box
+        p="4%"
+        minH={{ base: "35vh", md: "20vh", lg: "100vh" }}
+        backgroundImage={{ base: "none", lg: "/bm2.png" }}
+        bgPos="center"
+        bgRepeat="no-repeat"
+        bgSize="cover"
+        display={"flex"}
+        flexDir={"column"}
+        justifyContent={"center"}
+        textAlign={{ base: "center", lg: "left" }}
+      >
+        <Stack width={{ base: "100%", xl: "500px" }} py="30px">
+          <Heading
+            fontSize={{
+              base: "24px",
+              lg: "62px",
+            }}
+            color="#393E46"
+          >
+            Digital Marketing Agency With A Swag
+          </Heading>
+          <Text>
+            Sed ut perspiciatis unde omnis iste natus error voluptatem
+            laudantium totam aperiam
+          </Text>
+          <Button colorScheme={"blue"}>Make Booking</Button>
+        </Stack>
+      </Box>
+    </>
+  );
+}
+
+export default Hero;
